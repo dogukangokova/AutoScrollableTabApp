241bab587134f7b0ccb87f45b3a00166b886c885
Doğukan GÖKOVA <info@dogukangokova.me>
2023-02-23 15:51:15 +0300

    Initial Commit

---
+++ AutoScrollableTabApp/AutoScrollableTabAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoScrollableTabAppApp.swift
+//  AutoScrollableTabApp
+//
+//  Created by Devinely on 23.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct AutoScrollableTabAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
